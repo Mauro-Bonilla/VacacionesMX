ed780c6e5152f28da8907055410deed31a88c5d9
Mauro-Bonilla <maurobonillaolea@outlook.com>
2025-04-12 11:19:54 -0600

    user context + hooks + serversideroutes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
 import './ui/global.css';
+import { getCurrentUser } from '@/app/utils/get-current-user';
+import { AuthProvider } from './lib/context/auth-context';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getCurrentUser();
+  
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthProvider initialUser={user || null}>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
